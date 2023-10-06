521d9e1b7d9d6e1f75bb68a5fde6f4321082b37e
Doma1612 <dominik.martens@outlook.com>
2023-10-06 09:32:35 +0200

    create maven project and rest module

---
+++ video-portal-backend/SP_Video_Portal_REST/src/main/java/org/SP_Video_Portal_REST/App.java
@@ -0,0 +1,13 @@
+package org.SP_Video_Portal_REST;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
